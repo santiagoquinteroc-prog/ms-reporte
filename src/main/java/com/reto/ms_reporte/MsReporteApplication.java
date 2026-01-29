9f938665390a93739b0960d731ae7a5c9fa6a8cf
Santiago Quintero <santiago.quinteroc@pragma.com.co>
2026-01-29 18:02:44 -0500

    chore: init ms-reporte spring initializr

---
+++ src/main/java/com/reto/ms_reporte/MsReporteApplication.java
@@ -0,0 +1,13 @@
+package com.reto.ms_reporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsReporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsReporteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reto/ms_reporte/MsReporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reto.ms_reporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsReporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
